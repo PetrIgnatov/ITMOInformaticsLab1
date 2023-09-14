7e48c128030ace0be3a11ebc07ed734fd8c1654d
PetrIgnatov <144905118+PetrIgnatov@users.noreply.github.com>
2023-09-14 23:55:14 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Main
 {
+	public static int fact(int n)
+	{
+		int f = 1;
+		for (int i = 2; i <= n; i++)
+		{
+			f*=i;
+		}
+		return f;
+	}
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
@@ -26,6 +35,17 @@ public class Main
 						dec += (int)(inpnum.toCharArray()[i]-'A'+10)*fib[inpnum.length()-1-i];
 					}
 					break;
+				case "fact":
+					if (inpnum.toCharArray()[i] >= '0' && inpnum.toCharArray()[i] <= '9')
+                                        {
+                                                dec += (int)(inpnum.toCharArray()[i]-'0')*fact(inpnum.length()-i);
+                                        }
+                                        else
+                                        {
+                                                dec += (int)(inpnum.toCharArray()[i]-'A'+10)*fact(inpnum.length()-i);
+                                        }
+
+					break;
 				default:
 					if (inpnum.toCharArray()[i] >= '0' && inpnum.toCharArray()[i] <= '9')
 					{
@@ -39,19 +59,40 @@ public class Main
 			}
 		}
 		String ans = "";
-		while (dec > 0)
-		{
-			int cur = dec%(int)Double.parseDouble(finsys);
-			if (cur < 10)
-			{
-				ans = (char)('0'+cur)+ans;
-			}
-			else
-			{
-				ans = (char)('A'+cur-10)+ans;
-			}
-			dec /= (int)Double.parseDouble(finsys);
-		}
+		int pos = fib.length-1;
+		switch(finsys)
+                {
+               		 case "fib":
+				while (pos >= 0)
+				{
+        	                	if (dec >= fib[pos])
+                           		{
+						dec -= fib[pos];
+                                       		ans = ans + '1';
+					}
+					else if (ans != "")
+					{
+						ans = ans + '0';
+					}
+					pos--;
+				}
+                                break;
+			default:
+				while (dec > 0)
+				{
+                                	int cur = dec%(int)Double.parseDouble(finsys);
+                                	if (cur < 10)
+                                	{
+                                        	ans = (char)('0'+cur)+ans;
+                             		}
+					else
+                                	{
+                                       		ans = (char)('A'+cur-10)+ans;
+                                	}
+                               		dec /= (int)Double.parseDouble(finsys);
+				}
+                 		break;
+                }
 		System.out.println(ans);
 	}
 }
